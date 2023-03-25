9957628241992300c0659dc349ce0ec230c2650b
Aditya Kumar <aaz22hsns@gmail.com>
2023-03-25 19:08:24 +0530

    Initial commit

---
+++ imageprocessing.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import numpy as np
+from collections import Counter
+from matplotlib.colors import rgb2hex
+
+
+def rgb_to_hex(r, g, b):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+
+class ProcessImage():
+    def __init__(self):
+        pass
+
+    def get_top_10(self, imageLocation):
+
+        image = Image.open(f"./{imageLocation}")
+
+        list1 = []
+        numpydata = np.asarray(image)
+
+        for n in numpydata:
+            for m in n:
+                list1.append(f'[{m[0]}, {m[1]}, {m[2]}]')
+
+        new_set = set(list1)
+        words = {}
+        c = Counter(list1)
+        for n in new_set:
+            words.__setitem__(n, c[n])
+
+        top_10_colors = sorted(words.items(), key=lambda x: x[1], reverse=True)[0:10]
+        new_list = []
+        for n in top_10_colors:
+            l = n[0].replace('[', '')
+            l = l.replace(']', '').split(',')
+            l = [int(x) for x in l]
+            new_list.append(l)
+
+        hex_codes = [rgb_to_hex(n[0], n[1], n[2]) for n in new_list]
+
+        return hex_codes
---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+
+from flask import Flask, render_template, request
+from wtforms import FileField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from werkzeug.utils import secure_filename
+from imageprocessing import ProcessImage
+import os
+
+app = Flask(__name__)
+app.secret_key = "asdklfjoooiwjedlkjxcvmkjlfs"
+process_image = ProcessImage()
+
+
+class GetPhoto(FlaskForm):
+    photo = FileField(validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    form = GetPhoto()
+    colors = []
+    if request.method == "POST" and form.validate_on_submit:
+        image = request.files['photo']
+        image_name = image.filename
+        image.save(secure_filename(filename=image_name))
+        top10 = process_image.get_top_10(image_name)
+        colors = top10
+        time.sleep(2)
+        if os.path.isfile(f"./{image_name}"):
+            os.remove(f"./{image_name}")
+
+    return render_template('home.html', form=form, colors=colors)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
